662acf8939043cea41eb8ce81bd7e48b4de344d8
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-07-20 15:52:50 -0700

    Lots of changes to including more games with altering the path in the wombat scaper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715224155) do
+ActiveRecord::Schema.define(version: 20180720173940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180715224155) do
     t.string "game_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "recommendations", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@ review_data['the_articles'].each do |review|
   next if review['article'].nil? || review['article'].empty?
 
   tentative_title = review['article'][0]['titles']
+  tentative_image = review['article'][0]['image']
 
   next if tentative_title.empty? || tentative_title.nil?
 
-  # TODO: Need to simplfy way that title is set, consider using .first for an array to get first occuring title else if the first element is not the one we want (why??), we will use the long method
+  next if tentative_image.nil?
 
   if tentative_title.nil? || tentative_title.length.nil?
     title = 'untitled'
@@ -20,7 +21,13 @@ review_data['the_articles'].each do |review|
     title = tentative_title.group_by(&:itself).values.max_by(&:size).first
   end
 
-  new_game = Game.find_or_create_by!(game_title: title)
+  if tentative_image.nil?
+    image = 'no image'
+  else
+    image = tentative_image
+  end
+
+  new_game = Game.find_or_create_by!(game_title: title, image: image)
 
   # The content is broken up into separate index based on paragraphs. Joining them together at a line break
   content = review['article'][0]['content']
@@ -39,11 +46,12 @@ review_data['the_articles'].each do |review|
     TAG_WORDS.each do |tag_word|
       if new_review.content.include?(tag_word)
         new_tag = Tag.find_or_create_by!(tag_word: tag_word)
-        # NOTE: Can I combine the line above and below
+
         new_review.tags << new_tag
       end
     end
   rescue
     binding.pry
+    # TODO: replace with error/status handling
   end
 end
